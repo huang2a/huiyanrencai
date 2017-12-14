b774f23c5a1530d4d1748e58dda664c2b132c719
gy18505 <gy18505@163.com>
2017-12-14 18:17:41 +0800

    bugs fixed
    daily update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(function(){
 
+  // dropdown close
+  $('body').on('click',function(e){
+    var target=e?e.target:window.event.srcElement;
+    if(!$(target).parents().is('.navbar-nav')){
+      $('.dropdown.open').removeClass('open');
+    }
+  })
+
   $('.dropdown-toggle','.navbar').click(function(){
     $(this).parents('.dropdown').toggleClass('open');
   })
@@ -8,7 +16,10 @@ $(function(){
   $(".m-menu").mmenu({
     "extensions": [
         "pagedim-black"
-    ]
+    ],
+    navbar:{
+      title: "慧眼人才"
+    }
   });
   var API = $(".m-menu").data( "mmenu" );
   $(".navbar-toggle").click(function() {
