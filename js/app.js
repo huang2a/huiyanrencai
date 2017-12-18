6195e073c40c32755c30a2b4dbcee2e9e51ce65c
huang2a <1055198140@qq.com>
2017-12-18 16:08:49 +0800

    eidt mmenu
    改变手机菜单样式

--- js/app.js
+++ js/app.js
@@ -12,18 +12,10 @@ $(function(){
     $(this).parents('.dropdown').toggleClass('open');
   })
 
-  // mmenu init
-  $(".m-menu").mmenu({
-    "extensions": [
-        "pagedim-black"
-    ],
-    navbar:{
-      title: "慧眼人才"
-    }
-  });
-  var API = $(".m-menu").data( "mmenu" );
-  $(".navbar-toggle").click(function() {
-    API.open();
-  });
-  
 })
+
+$(function() {
+    $('#menu').mmenu({
+      //slidingSubmenus: false
+    });
+});
